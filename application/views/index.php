e5b2d29e84ddd9f968b3c5398c514b2b53fd75f2
jeyakanth thangam <jeyakanth0810@gmail.com>
2018-02-15 17:42:24 +0530

    Update main page

---
+++ application/views/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/css/style.css" />
+</head>
+<body>
+    <div class="headerPage">
+        <div class="navBar">
+            <div class="headerLogo">
+                    <img src="<?=base_url()?>assets/images/logo.png" class="headerLogoImg"/>
+            </div>
+            <div class="headerMenu">
+                    <ul class="headerMenuUl">
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><img src="<?=base_url()?>assets/images/search.png" class="headerSearch"/></li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+   <div class="bodyPage">
+        <div class="sliderPage">
+                sss
+
+        </div>
+   </div>
+   <div class="footer   Page">
+   
+   </div>
+</body>
+</html>
